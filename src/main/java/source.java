f6b92dbd4f79ea1eece8d3966041ef3b05d392fe
Danil <danila.gavrilov.96@mail.ru>
2021-03-18 23:51:07 +0300

    Created the main idea

--- src/main/java/source.java
+++ src/main/java/source.java
@@ -1,18 +1,22 @@
-import java.io.IOException;
+
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 
-public class mysource {
+public class source {
     public static void main(String[] args) {
 
+        int id = 227995590, count = 1;
+        String token = "a6304274a6304274a630427449a646a8a8aa630a6304274c6615027e743748aff31bf1d";
+
         HttpClient client = HttpClient.newHttpClient();
-        int id = 227995590;
+        String url = "https://api.vk.com/method/wall.get?owner_id=" + id + "&count=" + count + "&access_token=" + token + "&v=5.52";
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://api.vk.com/method/wall.get?owner_id=" + id + "&count=1&access_token=a6304274a6304274a630427449a646a8a8aa630a6304274c6615027e743748aff31bf1d&v=5.52"))
+                .uri(URI.create(url))
                 .build();
+
         client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
                 .thenApply(HttpResponse::body)
                 .thenAccept(System.out::println)
