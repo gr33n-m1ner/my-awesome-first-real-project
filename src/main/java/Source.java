ba368de7c230a54a16e2911c4a316a43fd8c8743
Danil <danila.gavrilov.96@mail.ru>
2021-04-27 22:02:16 +0300

    Renamed some variables, made a better algorithm of checking inputs

--- src/main/java/Source.java
+++ src/main/java/Source.java
@@ -8,20 +8,19 @@ import java.net.http.HttpResponse;
 
 
 public class Source {
-
     public static HttpClient client = HttpClient.newHttpClient();
     public static String token = "a6304274a6304274a630427449a646a8a8aa630a6304274c6615027e743748aff31bf1d";
 
-    public static boolean isInt(String a) {
-        boolean prov = true;
-        char [] q = a.toCharArray();
-        for (int i = 0; i < a.length(); ++i) {
-            prov = prov && (int) q[i] >= (int) '0' && (int) q[i] <= (int) '9';
+    public static boolean isInt(String input) {
+        boolean checkInt = true;
+        char [] q = input.toCharArray();
+        for (int i = 0; i < input.length(); ++i) {
+            checkInt = checkInt && (int) q[i] >= (int) '0' && (int) q[i] <= (int) '9';
         }
-        return prov;
+        return checkInt;
     }
 
-    private static String getWallPosts(String ownerId, int count) {
+    private static String getWallPosts(String ownerId, String count) {
 
         String url = "https://api.vk.com/method/wall.get?owner_id=" + ownerId + "&count=" + count + "&access_token=" + token + "&v=5.52";
         HttpRequest request = HttpRequest.newBuilder()
@@ -37,29 +36,32 @@ public class Source {
         // 227995590
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("ID страницы, с которой вы хотите взять посты");
-        String id;
-        while (true) {
-            String prov = in.readLine();
-            if (!isInt(prov) || prov.length() != 9) {
-                System.out.println("Id должен состоять из 9 цифр. Попробуйте еще раз");
-                continue;
-            }
-            id = prov;
-            break;
-        }
-        System.out.println("Количество постов");
-        int count;
-        while (true) {
-            String prov = in.readLine();
-            if (!isInt(prov)) {
-                System.out.println("Количество постов нужно задавать числом. Попробуйте еще раз");
-                continue;
-            }
-            count = Integer.parseInt(prov);
-            break;
-        }
-        String response;
+        String id = null;
 
+        boolean correctInput = false;
+        boolean ifIncorrect = false;
+        while (!correctInput) {
+            if (ifIncorrect) System.out.println("Id должен состоять из 9 цифр. Попробуйте еще раз");
+            String input = in.readLine();
+            correctInput = isInt(input) && input.length() == 9;
+            ifIncorrect = true;
+            id = input;
+        }
+
+        System.out.println("Количество постов");
+        String count = null;
+
+        correctInput = false;
+        ifIncorrect = false;
+        while (!correctInput) {
+            if (ifIncorrect) System.out.println("Количество постов нужно задавать числом. Попробуйте еще раз");
+            String input = in.readLine();
+            correctInput = isInt(input);
+            ifIncorrect = true;
+            count = input;
+        }
+        
+        String response;
         response = getWallPosts(id, count);
         System.out.println(response);
 
